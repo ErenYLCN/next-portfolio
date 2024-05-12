80cb91f01dfd52b3412d5fa259a315e2cc3bacd4
Eren Yalçın <eren@hipo.biz>
2024-05-12 17:32:20 +0300

    feat(component): add `BackgroundGradient`

---
+++ src/component/background-gradient/BackgroundGradient.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function BackgroundGradient() {
+  return (
+    <>
+      <div
+        className="bg-[#fbe2e3] absolute top-[-6rem] 
+        -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] 
+        rounded-full blur-[10rem] sm:w-[68.75rem]"
+      />
+
+      <div
+        className="bg-[#dbd7fb] absolute top-[-1rem] -z-10 
+        left-[-35rem] h-[31.25rem] w-[50rem] rounded-full 
+        blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] 
+        lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"
+      />
+    </>
+  );
+}
+
+export default BackgroundGradient;
