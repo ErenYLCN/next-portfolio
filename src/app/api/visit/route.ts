fd3c2b6aa74491c238b55def153c2ef8294c1566
Eren Yalçın <eren@hipo.biz>
2024-06-03 13:05:12 +0300

    fix: try another way to access metrics.json

--- src/app/api/visit/route.ts
+++ src/app/api/visit/route.ts
@@ -2,22 +2,17 @@ import fs from "fs"
 
 // increment visit_count on public/metrics.json
 export async function POST() {
-  const metrics = JSON.parse(fs.readFileSync(process.env.NODE_ENV === "development"
-    ? "public/metrics.json"
-    : "/metrics.json"
-    , "utf-8"))
+  let path = process.cwd() + "/public/metrics.json"
+  const metrics = JSON.parse(fs.readFileSync(path, "utf-8"))
   metrics.visit_count += 1
-  fs.writeFileSync(process.env.NODE_ENV === "development"
-    ? "public/metrics.json"
-    : "/metrics.json", JSON.stringify(metrics, null, 2))
+  fs.writeFileSync(path, JSON.stringify(metrics, null, 2))
 
   return Response.json({ message: "incremented" })
 }
 
 // get visit_count from public/metrics.json
 export async function GET() {
-  const metrics = JSON.parse(fs.readFileSync(process.env.NODE_ENV === "development"
-    ? "public/metrics.json"
-    : "/metrics.json", "utf-8"))
+  let path = process.cwd() + "/public/metrics.json"
+  const metrics = JSON.parse(fs.readFileSync(path, "utf-8"))
   return Response.json({ visit_count: metrics.visit_count })
 }
